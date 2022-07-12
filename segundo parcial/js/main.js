8b58c15087d35b766635c06b49a26c7b7a97589f
pedbvasq <pedbvasq@espol.edu.ec>
2022-07-11 23:41:43 -0500

    dashboard-primera-version

--- segundo parcial/js/main.js
+++ segundo parcial/js/main.js
@@ -15,11 +15,62 @@ cargarDatos = () => {
 
             document.querySelector('div.input-group > select').addEventListener('change', (eventSelect) => {
 
-                const arr_Datos = data.data.standings.filter((datos) => {
+                const arr_frases = data.data.standings.filter((datos) => {
 
                     return datos.team.name == eventSelect.target.value
                 })
 
+                let games = arr_frases[0]["stats"][3]["displayName"]
+                let gamesN = arr_frases[0]["stats"][3]["value"]
+                let win =arr_frases[0]["stats"][0]["displayName"]
+                let winN =arr_frases[0]["stats"][0]["value"]
+                let loses = arr_frases[0]["stats"][1]["displayName"]
+                let losesN = arr_frases[0]["stats"][1]["value"]
+                let tie = arr_frases[0]["stats"][2]["displayName"]
+                let tieN =arr_frases[0]["stats"][2]["value"]
+
+
+              
+                let plantilla =
+            `<div class="col-lg-3">
+                <div class="test-inner ">
+                     <div class="test-author-thumb d-flex">
+                          <div class="test-author-info">
+                                <h4>${games}</h4>                                            
+                          </div>
+                     </div>
+                    <span>${gamesN}</span>
+                    <i class="f"></i>
+
+                     <div class="test-author-thumb d-flex">
+                          <div class="test-author-info">
+                                <h4>${win}</h4>                                            
+                          </div>
+                     </div>
+                    <span>${winN}</span>
+                    <i class="f"></i>
+                     <div class="test-author-thumb d-flex">
+                          <div class="test-author-info">
+                                <h4>${loses}</h4>                                            
+                          </div>
+                     </div>
+                    <span>${losesN}</span>
+                    <i class="f"></i>
+                     <div class="test-author-thumb d-flex">
+                          <div class="test-author-info">
+                                <h4>${tie}</h4>                                            
+                          </div>
+                     </div>
+                    <span>${tieN}</span>
+                    <i class="f"></i>
+
+
+
+              </div>
+
+            </div>`
+                document.getElementById("frases").innerHTML = plantilla;
+             
 
                
             })
