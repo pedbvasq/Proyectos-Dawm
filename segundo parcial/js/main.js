ec8f0fbe48e08f7f34a6147fcf3ea72a7688e291
pedbvasq <pedbvasq@espol.edu.ec>
2022-07-12 01:28:34 -0500

    dashboard2.0

--- segundo parcial/js/main.js
+++ segundo parcial/js/main.js
@@ -1,3 +1,4 @@
+
 cargarDatos = () => {
     fetch("https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2020&sort=asc")
         .then(response => response.json())
@@ -19,57 +20,80 @@ cargarDatos = () => {
 
                     return datos.team.name == eventSelect.target.value
                 })
-
-                let games = arr_frases[0]["stats"][3]["displayName"]
+                let nombre = document.getElementsByTagName('option')[eventSelect.target.selectedIndex].innerHTML;
+                let games = arr_frases[0]["stats"][3]["displayName"].innerHTML
                 let gamesN = arr_frases[0]["stats"][3]["value"]
-                let win =arr_frases[0]["stats"][0]["displayName"]
-                let winN =arr_frases[0]["stats"][0]["value"]
+                let win = arr_frases[0]["stats"][0]["displayName"]
+                let winN = arr_frases[0]["stats"][0]["value"]
                 let loses = arr_frases[0]["stats"][1]["displayName"]
                 let losesN = arr_frases[0]["stats"][1]["value"]
                 let tie = arr_frases[0]["stats"][2]["displayName"]
-                let tieN =arr_frases[0]["stats"][2]["value"]
+                let tieN = arr_frases[0]["stats"][2]["value"]
 
 
-              
+
                 let plantilla =
-            `<div class="col-lg-3">
+                    `<div class="col-lg-3">
                 <div class="test-inner ">
-                     <div class="test-author-thumb d-flex">
-                          <div class="test-author-info">
-                                <h4>${games}</h4>                                            
-                          </div>
-                     </div>
-                    <span>${gamesN}</span>
-                    <i class="f"></i>
+                     <table>
 
-                     <div class="test-author-thumb d-flex">
-                          <div class="test-author-info">
-                                <h4>${win}</h4>                                            
-                          </div>
-                     </div>
-                    <span>${winN}</span>
-                    <i class="f"></i>
-                     <div class="test-author-thumb d-flex">
-                          <div class="test-author-info">
-                                <h4>${loses}</h4>                                            
-                          </div>
-                     </div>
-                    <span>${losesN}</span>
-                    <i class="f"></i>
-                     <div class="test-author-thumb d-flex">
-                          <div class="test-author-info">
-                                <h4>${tie}</h4>                                            
-                          </div>
-                     </div>
-                    <span>${tieN}</span>
-                    <i class="f"></i>
+                  <caption> estadisticas del ${nombre} </caption>
 
+                        <thead>
+                             <tr>
+
+                                 <th scope="col"> ${win + "|"}<spam></spam> </th>
+                                  <th scope="col"> ${loses + "|"} </th>
+                                 <th scope="col"> ${tie}</th>
+                                </tr>
+                      </thead>
+
+                 <tbody>
+                        <tr>
+                        <td> ${winN} </td>
+                         <td> ${losesN} </td>
+                       <td> ${tieN} </td>
+                      </tr>
+                </tbody>
+
+             </table>
+
+
+              </div>
+
+            </div>`
+
+                let charC = `<div class="">
+                <div class="">
+                     <table class ="charts-css bar multiple show-labels show-primary-axis ">
+
+                  <caption> estadisticas del ${nombre} </caption>
+
+                        <thead>
+                             <tr>
+
+                                 <th scope="col"> ${win + "|"}<spam></spam> </th>
+                                  <th scope="col"> ${loses + "|"} </th>
+                                 <th scope="col"> ${tie}</th>
+                                </tr>
+                      </thead>
+
+                 <tbody>
+                        <tr>
+                        <td  style="--size: calc( ${winN}/ 38)"> ${winN} </td>
+                         <td style="--size: calc( ${losesN}/ 38)" > ${losesN} </td>
+                       <td style="--size: calc( ${tieN}/ 38)"> ${tieN} </td>
+                      </tr>
+                </tbody>
+
+             </table>
 
 
               </div>
 
             </div>`
                 document.getElementById("frases").innerHTML = plantilla;
+                document.getElementById("char").innerHTML = charC;
              
 
                
