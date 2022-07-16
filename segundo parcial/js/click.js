0f7694381fb707794750352de3b6d986176a643c
pedbvasq <pedbvasq@espol.edu.ec>
2022-07-15 20:02:23 -0500

    botonClick

--- segundo parcial/js/click.js
+++ segundo parcial/js/click.js
@@ -4,10 +4,24 @@ function traer() {
     fetch('https://api-football-standings.azharimm.site/leagues/eng.1/standings?season=2020&sort=asc')
         .then(res => res.json())
         .then(data => {
+
             let contenido = document.querySelector('#contenido')
-            for (let i = 0; i < 20; i++) {
-                console.log(data)
-            }
+
+            let numero = getRandomInt(21)
+            let id= data["data"]["standings"][numero]["team"]["name"]
+            let ab=data["data"]["standings"][numero]["team"]["abbreviation"]
+            let logo=data["data"]["standings"][numero]["team"]["logos"][0]["href"]
+
+            contenido.innerHTML = `
+            <img src="${logo}" width="200px" class="img-fluid rounded-circle"> 
+            <p>Equipo: ${id+"("+ab+")"}</p>
+            `
             
+
+
         })
 } 
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
