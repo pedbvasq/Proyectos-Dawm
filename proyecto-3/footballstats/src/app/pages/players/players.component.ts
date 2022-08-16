cfa95b988295b09592fbf72a66892c8202ab7260
pedbvasq <pedbvasq@espol.edu.ec>
2022-08-16 09:17:02 -0500

    select

--- proyecto-3/footballstats/src/app/pages/players/players.component.ts
+++ proyecto-3/footballstats/src/app/pages/players/players.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
-  styleUrls: ['./players.component.scss']
+  styleUrls: ['./players.component.scss'],
 })
+
 export class PlayersComponent implements OnInit {
 
   constructor() { }
